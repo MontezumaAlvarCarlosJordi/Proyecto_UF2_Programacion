8c7c2d1d721fd165efe9db307b1c488ffd8a6078
MontezumaAlvarCarlosJordi <60001553+MontezumaAlvarCarlosJordi@users.noreply.github.com>
2020-01-20 11:29:06 +0100

    Personaje actualizado y mapa empezado

--- Montezumas_Revenge/Montezumas_Revenge/map.cpp
+++ Montezumas_Revenge/Montezumas_Revenge/map.cpp
@@ -1,16 +1,24 @@
 #include "map.h"
 
-
+char map1[200][50];
 
 void DrawMap() {
-
-
+	for (int i = 0; i < W; i++)
+	{
+		for (int j = 0; j < H; j++)
+			if (map1[i][j] == 'X')
+				FASG::WritePixelBuffer(i, j, FASG::EBackColor::LightGreen);
+	}
+	
 
 }
 
 void InitMap()
 {
+	
 
-
-
+	for (int i = 0; i < 200; i++)
+	{
+		map1[i][30] = 'X';
+	}
 }
