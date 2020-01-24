a4a40c2c40c3130047898502fffafdb7a6dfe234
MontezumaAlvarCarlosJordi <60001553+MontezumaAlvarCarlosJordi@users.noreply.github.com>
2020-01-24 14:00:51 +0100

    cosas

--- Montezumas_Revenge/Montezumas_Revenge/assets.h
+++ Montezumas_Revenge/Montezumas_Revenge/assets.h
@@ -1,10 +1,8 @@
 #pragma once
 #include "FAriasSimpleGraphics.h"
 
-//Espacio de nombres por defecto
 using namespace FASG;
 
-//Variables del jugador
 struct Assets {
 
 	Sprite sprite[6] = { Sprite("key.txt") };
